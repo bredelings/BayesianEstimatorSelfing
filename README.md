0d7aee5c8a50c4133ba30cec6b6aae8b3d38604f
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2015-03-13 12:12:43 -0400

    Add basic files.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BayesianEstimatorSelfing
 A Bayesian method for estimating self-fertilization rates and other mating system parameters
+
+## Quick Start
+
+% bali-phy -m Selfing.hs --iter=10000 --- Examples/outfile.001.70.001.phase &
+% statreport --select=Selfing.s --mode --HPD Selfing-1/C1.p
+% tracer Selfing-1/C1.p
---
+++ RobustSelfing.hs
@@ -0,0 +1,36 @@
+module Herm where
+{
+import PopGen;
+import Distributions;
+import System.Environment;
+
+observed_alleles = read_phase_file (getArgs!!0);
+
+n_loci = length observed_alleles;
+
+n_individuals = length (observed_alleles!!0)/2;
+
+main = Prefix "Selfing" $ do 
+{
+-- Uncomment to estimate alpha:
+--  alpha <- gamma 0.5 0.05;
+--  Log "alpha" alpha;
+  let {alpha = 0.10};
+
+  theta_effective <- dp n_loci alpha (gamma 0.25 2.0); 
+  -- Alternatively, one can use a dirichlet process mixture:
+  --  theta_effective <- dpm n_loci (gamma 0.5 1.0) (gamma 1.05 0.1); 
+
+  f <- uniform 0.0 1.0;
+                     
+  s <- uniform 0.0 1.0;
+
+  afs_dist <- robust_diploid_afs n_individuals n_loci s f theta_effective;
+
+  Observe observed_alleles afs_dist;
+
+  Log "s" s;
+  Log "f" f;
+  Log "theta*" theta_effective;
+};
+}
