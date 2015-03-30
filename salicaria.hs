1b6359ee00db7839ced278f8280b35627139e3cd
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2015-03-30 17:04:37 -0400

    Move details to gyno_mating_system.

--- salicaria.hs
+++ salicaria.hs
@@ -11,26 +11,25 @@ n_loci = length observed_alleles;
 
 n_individuals = length (observed_alleles!!0)/2;
 
+gyno_mating_system tau a p_f sigma = (s, h, r) where
+    {p_h = 1.0 - p_f;
+     s   = let {x1=tau*p_h*a; x2=p_h*(1.0-a); x3=p_f*sigma} in x1/(x1+x2+x3);
+     h   = let {n2 = p_h * (1.0-a);n3 = p_f * sigma} in n2/(n2 + n3);
+     hh  = (1.0+h)/2.0;
+     r   = (2.0*s + (1.0-s)*(1.0+h))^2 /(4.0*p_h) + ((1.0-s)*(1.0-h))^2/(4.0*p_f)};
+
+
 gyno_model _ = Prefix "Gyno" $ do
 {
--- tau ~ beta( 16.0, 64.0 ); tau = 0.205;
   tau <- beta 2.0 8.0;
 
--- a = 1.0; a = 0.99; a = 0.95; a = 0.9; a = 0.8; a = 0.6; a = 0.5; a = 0.4;
   a <- uniform 0.0 1.0;
 
   p_f <- uniform 0.0 1.0;
 
   let {sigma = 1.0};
 
-  let {p_h = 1.0 - p_f};
-  
-  let {s = let {x1=tau*p_h*a; x2=p_h*(1.0-a); x3=p_f*sigma} in x1/(x1+x2+x3)};
-
-  let {h = let {n2 = p_h * (1.0-a);n3 = p_f * sigma} in n2/(n2 + n3)};
-
-  let {hh = (1.0+h)/2.0;
-        r = (2.0*s + (1.0-s)*(1.0+h))^2 /(4.0*p_h) + ((1.0-s)*(1.0-h))^2/(4.0*p_f)};
+  let {(s, h, r) = gyno_mating_system tau a p_f sigma};
 
   return (tau, a, p_f, sigma, s, h, r);
 };
