ecd23989f17982f664ee56920bc64074398f0f65
Benjamin Redelings <benjamin_redelings@ncsu.edu>
2015-06-15 14:52:52 -0400

    Further fix new andro_mating_system2 functions.

--- files/modules/PopGen/Selfing/Androdioecy.hs
+++ files/modules/PopGen/Selfing/Androdioecy.hs
@@ -8,13 +8,13 @@ andro_mating_system s' tau p_m = (s,r) where {s = (tau*s')/(tau*s'+1.0-s');
                                               r = andro_mating_system' s p_m};
 
 -- By ignoring s' and tau, we are focusing on just the adult selfing rate s'
-andro_mating_system2' s p_m sigma = (s,r) where {p_h = 1.0 - p_m;                 -- fraction of hermaphrodites
-                                                 mp = p_m*sigma;                 -- male pollen
-						 hp = p_h;                       -- herm pollen
-		     	    	      	      	 g_m = 0.5*(1.0-s)*mp/(mp+hp);   -- probability a parent is male
-						 g_h = 1.0 - g_m;                 -- probability a parent is herm
-						 rr = (g_h^2)/p_h + (g_m^2)/p_m; -- N/N*, where N=N_m + N_h
-						 r = 1.0/r};                     -- N*/N
+andro_mating_system2' s p_m sigma = r where {p_h = 1.0 - p_m;                 -- fraction of hermaphrodites
+                                             mp = p_m*sigma;                 -- male pollen
+					     hp = p_h;                       -- herm pollen
+		     	    	      	     g_m = 0.5*(1.0-s)*mp/(mp+hp);   -- probability a parent is male
+					     g_h = 1.0 - g_m;                 -- probability a parent is herm
+					     rr = (g_h^2)/p_h + (g_m^2)/p_m; -- N/N*, where N=N_m + N_h
+					     r = 1.0/rr};                     -- N*/N
 
 -- By computing s from s', we are parameterizing the adult selfing rate in terms of the seed selfing rate and viability
 andro_mating_system2 s' tau p_m sigma = (s,r) where {s = (tau*s')/(tau*s'+1.0-s');
