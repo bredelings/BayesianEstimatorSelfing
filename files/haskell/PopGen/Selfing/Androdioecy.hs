43fa760006794307f5d0afe9cf6e53af49f7af20
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-08-17 19:54:45 -0700

    Remove unnecessary ".0" in several places.

--- files/haskell/PopGen/Selfing/Androdioecy.hs
+++ files/haskell/PopGen/Selfing/Androdioecy.hs
@@ -1,24 +1,24 @@
 module PopGen.Selfing.Androdioecy where
 
 -- By ignoring s' and tau, we are focusing on just the adult selfing rate s'
-andro_mating_system' s p_m = 1.0 / ((1.0 + s) ^ 2 / (4.0 * p_h) + (1.0 - s) ^ 2 / (4.0 * p_m)) where p_h = 1.0 - p_m
+andro_mating_system' s p_m = 1 / ((1 + s) ^ 2 / (4 * p_h) + (1 - s) ^ 2 / (4 * p_m)) where p_h = 1 - p_m
 
 -- By computing s from s', we are parameterizing the adult selfing rate in terms of the seed selfing rate and viability
 andro_mating_system s' tau p_m = (s, r)  where
-    s = (tau * s') / (tau * s' + 1.0 - s')
+    s = (tau * s') / (tau * s' + 1 - s')
     r = andro_mating_system' s p_m
 
 -- By ignoring s' and tau, we are focusing on just the adult selfing rate s'
 andro_mating_system2' s p_m sigma = r  where
-    p_h = 1.0 - p_m                -- fraction of hermaphrodites
-    mp  = p_m * sigma                 -- male pollen
+    p_h = 1 - p_m                   -- fraction of hermaphrodites
+    mp  = p_m * sigma               -- male pollen
     hp  = p_h                       -- herm pollen
-    g_m = 0.5 * (1.0 - s) * mp / (mp + hp)   -- probability a parent is male
-    g_h = 1.0 - g_m                -- probability a parent is herm
+    g_m = 0.5 * (1 - s) * mp / (mp + hp)   -- probability a parent is male
+    g_h = 1 - g_m                   -- probability a parent is herm
     rr  = (g_h ^ 2) / p_h + (g_m ^ 2) / p_m -- N/N*, where N=N_m + N_h
-    r   = 1.0 / rr                     -- N*/N
+    r   = 1 / rr                    -- N*/N
 
 -- By computing s from s', we are parameterizing the adult selfing rate in terms of the seed selfing rate and viability
 andro_mating_system2 s' tau p_m sigma = (s, r)  where
-    s = (tau * s') / (tau * s' + 1.0 - s')
+    s = (tau * s') / (tau * s' + 1 - s')
     r = andro_mating_system2' s p_m sigma
